ef63a5bc9dd2b88f7d6d9fd7eddd8aa1b6eba1cb
INTERN\Mal824 <lukas.nauerth@students.hs-mainz.de>
2023-01-06 14:00:59 +0100

    Erster Ansatz (I'm sorry B))

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float horizontalInput;
+     private float forwardInput;
+     [SerializeField] float turnSpeed;    
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Spielerbewegung (hast du mir gezeigt lol)
+    void Update()
+    {
+      horizontalInput = Input.GetAxis("Horizontal");  
+      transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+       forwardInput = Input.GetAxis("Vertical"); 
+       transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+    }
+}
