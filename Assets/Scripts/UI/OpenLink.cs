788d9667f313f0c91751703fa1ec814485ad7151
Scroll26 <52886221+Scroll26@users.noreply.github.com>
2023-01-30 17:48:58 +0100

    added TextMeshPro

---
+++ Assets/Scripts/UI/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenFeslyanStudios()
+    {
+        Application.OpenURL("https://www.fesliyanstudios.com");
+    }
+}
